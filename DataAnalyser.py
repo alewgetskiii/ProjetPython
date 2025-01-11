b38094e4719cc06564b4d067a414a21ddd801e25
alewgetskiii <138591611+alewgetskiii@users.noreply.github.com>
2025-01-11 18:49:57 +0100

    Update DataAnalyser.py
    modif causal

--- DataAnalyser.py
+++ DataAnalyser.py
@@ -94,104 +94,25 @@ class DataAnalyser:
         return res
 
     def causal(self, variables, max_lag, p_value_limit):
-        results = []
-        lag_matrix = {var: [None] * max_lag for var in variables}  # Dictionnaire pour stocker les p-values par lag
-
-        ''' on teste la causalite des autres variables sur les returns du coffee'''
+        triplets = []
         for var in variables:
             returns_var = np.array(self.getColReturns(var)[self.getColReturns(var).index >= '1991-12-31'])
             data = pd.DataFrame({'y': self.getAnnualCoffeeReturn()[:len(returns_var)], 'x': returns_var})
             try:
                 test_result = grangercausalitytests(data[['y', 'x']], max_lag, verbose=False)
-                p_values = [test_result[lag][0]['ssr_ftest'][1] for lag in range(1, max_lag + 1)]
-            
-                # Filtrage des p-values inférieures à la limite
-                valid_p_values = [(var, lag, p_value) for lag, p_value in enumerate(p_values, start=1) if p_value < p_value_limit]
-                for lag, p_value in enumerate(p_values, start=1):
-                    lag_matrix[var][lag-1] = p_value
-
-                if valid_p_values:
-                    # Ajouter les p-values valides dans la liste des résultats
-                    results.extend(valid_p_values)
-        
+                for lag in range(1, max_lag + 1):
+                    p_value = test_result[lag][0]['ssr_ftest'][1]
+                    f_stat = test_result[lag][0]['ssr_ftest'][0]
+                    if p_value < p_value_limit:  # On ne garde que les p-values sous la limite
+                        triplets.append((var, lag, p_value, f_stat))
             except Exception as e:
                 print(f"Erreur avec la colonne {var}: {e}")
-        
-        '''Trier les résultats par p_value (croissant)'''
-        sorted_results = sorted(results, key=lambda x: x[2])
-        seen_first_values = set()
-
-        '''Parcours des triplets'''
-        unique_results = []
-        for triplet in sorted_results:
-            first_value = triplet[0]
-            if first_value not in seen_first_values:
-                unique_results.append(triplet)
-                seen_first_values.add(first_value)
-
-        # Convertir le dictionnaire en DataFrame
-        lag_df = pd.DataFrame(lag_matrix)
-        # Afficher la matrice des p-values pour chaque variable et lag
-        print("Matrice filtree des p-values pour chaque variable et lag :")
-        print(lag_df)
-
-        return unique_results
-
-    def RandomForest(self, X_daily, X_annual):
-        valid_columns = [col for col in X_daily + X_annual if col in self._returns.columns]
-        X_combined = self._returns[valid_columns]  
-        X_combined = X_combined.ffill()  
-        X_combined = X_combined.fillna(0)
-        print(X_combined)
-        y = self._returns['r_coffee']
-        X_train, X_test, y_train, y_test = train_test_split(X_combined, y, test_size=0.2, random_state=42)
-
-        param_grid = {
-            'n_estimators': [100, 200, 300],
-            'max_depth': [None, 10, 20, 30],
-            'min_samples_split': [2, 5, 10],
-            'min_samples_leaf': [1, 2, 4],
-            'max_features': ['auto', 'sqrt', 'log2']
-        }
-
-        #grid_search = GridSearchCV(RandomForestRegressor(random_state=42), param_grid, cv=5, scoring='neg_mean_squared_error')
-        #grid_search.fit(X_train, y_train)
-
-        #best_model = grid_search.best_estimator_
-        #print("Best parameters:", grid_search.best_params_)
-        model = RandomForestRegressor(max_depth= 10, max_features= 'sqrt', min_samples_leaf= 4, min_samples_split=2, n_estimators=300, random_state=42)
-        #model = XGBRegressor(max_depth= 10, max_features= 'sqrt', min_samples_leaf= 4, min_samples_split=2, n_estimators=300, random_state=42)
-        #model = Ridge(alpha=1.0)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test) 
-
-        r2 = r2_score(y_test, y_pred)
-        mae = mean_absolute_error(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = np.sqrt(mse)
-
-
-        # Affichage des métriques
-        
-        print(f"R-squared: {r2:.4f}")
-        print(f"Mean Absolute Error (MAE): {mae:.4f}")
-        print(f"Mean Squared Error (MSE): {mse:.4f}")
-        print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
-        
-        plt.figure(figsize=(10, 6))
-        plt.scatter(y_test, y_pred, alpha=0.7, color='b', label='Prédictions')
-        plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='r', linestyle='--', label='Idéal')
-        plt.xlabel("Valeurs Réelles")
-        plt.ylabel("Valeurs Prédites")
-        plt.title("Prédictions vs Valeurs Réelles")
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-        # Retourner éventuellement les scores pour utilisation ultérieure
-        return r2, mae, mse, rmse
-
     
+        # Trier les triplets par p-value croissante
+        triplets = sorted(triplets, key=lambda x: x[3])
+        return triplets[:10]
+
+   
     def plotVariable(self, col):
         data = self.getValue(col)
         fig, (ax1) = plt.subplots(1, 1, figsize=(12, 12))
